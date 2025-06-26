1c678c104bcda27e40e4711d8fa6d96b600244e5
InvalidFunction0 <ayaan.waqas@outlook.com>
2025-06-26 15:44:58 +0100

    Quotes page, styling is external

--- app/routes/quote.tsx
+++ app/routes/quote.tsx
@@ -3,7 +3,12 @@ import styles from "app/styles/routes/quote.module.css";
 export default function Quote() {
   return (
     <div className={styles.container}>
-      <h1 className={styles.title}>Quotes</h1>
+      <h1 className={styles.title}>Get a Quote or Book a Chauffeur</h1>
+
+      <iframe
+        src="https://www.cdserver1.com/WebBooking/WebBooking5.aspx?ID=2af9c03f-5ff7-4440-9bbe-7820e0d49637"
+        className={styles.cdsForm}
+      />
     </div>
   );
 }
