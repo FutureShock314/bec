dc1b08ed2f5452d0eb54d52cfdbfd53cdbe069fc
InvalidFunction0 <ayaan.waqas@outlook.com>
2025-06-26 16:11:51 +0100

    no booking for you

--- app/routes/quote.tsx
+++ app/routes/quote.tsx
@@ -3,7 +3,7 @@ import styles from "app/styles/routes/quote.module.css";
 export default function Quote() {
   return (
     <div className={styles.container}>
-      <h1 className={styles.title}>Get a Quote or Book a Chauffeur</h1>
+      <h1 className={styles.title}>Get a Quote</h1>
 
       <iframe
         src="https://www.cdserver1.com/WebBooking/WebBooking5.aspx?ID=2af9c03f-5ff7-4440-9bbe-7820e0d49637"
