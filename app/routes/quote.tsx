3e70aa7b7d456abc4e00013f688cc6583691c3d1
InvalidFunction0 <ayaan.waqas@outlook.com>
2025-06-26 14:41:33 +0100

    reformat to use 2-space indenting in css

--- app/routes/quote.tsx
+++ app/routes/quote.tsx
@@ -1,9 +1,9 @@
-import styles from 'app/styles/routes/quote.module.css';
+import styles from "app/styles/routes/quote.module.css";
 
 export default function Quote() {
   return (
-    <>
-      <h1>Quotes</h1>
-    </>
-  )
+    <div className={styles.container}>
+      <h1 className={styles.title}>Quotes</h1>
+    </div>
+  );
 }
